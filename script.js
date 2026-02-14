299cb6e5e262ff2f795f9ef092519abac650b3fb
Mussavir ul haq <mussavirulhaq@gmail.com>
2026-02-14 16:05:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -300,7 +300,7 @@ async function savePaymentToMongo() {
 
     try {
         // IMPORTANT: Use relative URL for hosting on Vercel/Render
-        const response = await fetch('http://localhost:3000/save-payment', {
+        const response = await fetch('/api/save-payment', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(paymentData)
@@ -342,3 +342,4 @@ async function savePaymentToMongo() {
     }
 
     init();
+
